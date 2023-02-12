bcc6f76c5c0225389f34dda6899ffbe89db2f39d
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2023-02-12 21:50:20 +0300

    Конфигурация приложения (Application Configuration)

---
+++ config.py
@@ -0,0 +1,11 @@
+FILE_TYPE = {
+    'Torrent': ['torrent'],
+    'Music': ['mp3', 'wav', 'wma', 'ogg', 'aac', 'flac'],
+    'Video': ['mp4', 'avi', 'mkv', 'mov', 'flv', 'vob'],
+    'Document Word': ['docx', 'doc', 'rtf', 'xlsx', 'pdf'],
+    'Document Notebook': ['txt'],
+    'Install File': ['exe', 'msi'],
+    'Document HTML': ['html'],
+    'Archive(RAR)': ['rar', 'zip'],
+    'Images': ['ico', 'jpg', 'gif', 'png', 'svg', 'bmp', 'jpeg', 'tiff']
+}
